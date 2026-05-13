d1500e4f816d57adb60b8927ee6018f281c34ddb
Muhammad Siddiq Shah <siddiqshah478@gmail.com>
2026-05-13 20:27:25 +0500

    Fix UI and Complete HomePage

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,35 +1,127 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const types = ["buy", "rent"]
+const types = ["buy", "rent"];
 
 const SearchBar = () => {
+  const [query, setQuery] = useState({
+    type: "buy",
+    city: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
 
-    const [query, setQuery] = useState({
-        type: "buy",
-        city: "",
-        minprice: 0,
-        maxprice: 0
-    })
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
 
-    
+  return (
+    <div className="w-full max-w-[900px]">
+      
+      {/* TYPES */}
+      <div className="flex">
+        {types.map((type, index) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={`
+              px-8 py-4
+              border border-[#999]
+              border-b-0
+              capitalize
+              transition-all
+              duration-300
 
-    return (
-        <div className='searchBar'>
+              ${
+                query.type === type
+                  ? "bg-black text-white"
+                  : "bg-white text-black"
+              }
 
-            <div className="type">
-                <button>Buy</button>
-                <button>Rent</button>
-            </div>
-            <form>
-                <input type="text" name="Localtion" placeholder='City Location' />
-                <input type="number" name="minPrice" min={0} max={10000000} placeholder='Min Price' />
-                <input type="number" name="maxPrice" min={0} max={10000000} placeholder='Max Price' />
-                <button>
-                    <img src="/search.png" alt="" />
-                </button>
-            </form>
-        </div>
-    )
-}
+              ${index === 0 ? "rounded-tl-md border-r-0" : ""}
+              ${index === types.length - 1 ? "rounded-tr-md border-l-0" : ""}
+            `}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
 
-export default SearchBar
+      {/* FORM */}
+      <form
+        className="
+          border border-[#999]
+          flex
+          flex-col
+          md:flex-row
+          items-center
+          justify-between
+        "
+      >
+        
+        <input
+          type="text"
+          placeholder="City Location"
+          className="
+            w-full
+            md:w-[220px]
+            p-5
+            outline-none
+            border-b
+            md:border-b-0
+            md:border-r
+            border-[#999]
+          "
+        />
+
+        <input
+          type="number"
+          placeholder="Min Price"
+          className="
+            w-full
+            md:w-[200px]
+            p-5
+            outline-none
+            border-b
+            md:border-b-0
+            md:border-r
+            border-[#999]
+          "
+        />
+
+        <input
+          type="number"
+          placeholder="Max Price"
+          className="
+            w-full
+            md:w-[200px]
+            p-5
+            outline-none
+          "
+        />
+
+        <button
+          className="
+            w-full
+            md:w-[120px]
+            bg-[#fece51]
+            h-[70px]
+            flex
+            items-center
+            justify-center
+            hover:bg-yellow-400
+            transition-all
+            duration-300
+          "
+        >
+          <img
+            src="/search.png"
+            alt="search"
+            className="w-6 h-6"
+          />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
