c4ae034dc1c870fe2a12696ea397cbf2c06ba292
Muhammad Siddiq Shah <siddiqshah478@gmail.com>
2026-05-15 16:56:25 +0500

    Add login and logout feature

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import prisma from "../../lib/prisma.js"
 
 const register = async (req, res) => {
@@ -6,24 +7,74 @@ const register = async (req, res) => {
     const { username, email, password } = req.body
 
 
-    const hashPassword = await bcrypt.hash(password, 10)
+    try {
 
+        const hashPassword = await bcrypt.hash(password, 10)
+        const newUser = await prisma.user.create({
+            data: {
+                username, email, password: hashPassword
+            }
+        })
 
-    const newUser = await prisma.user.create({
-        data: {
-            username, email, password: hashPassword
-        }
-    })
-
-    console.log(newUser)
+        res.status(201).json({
+            message: "User created successfully"
+        })
+    } catch (err) {
+        console.log("err", err)
+        res.status(500).json({
+            message: "Failed request to create a user"
+        })
+    }
 }
 
-const login = (req, res) => {
-    res.send("Login")
+const login = async (req, res) => {
+
+    const { email, password } = req.body
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) return res.status(401).json({
+            message: "Invalid credentials"
+        })
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) return res.status(401).json({
+            message: "Invalid credentials"
+        })
+
+        const age = 1000 * 60 * 60 * 24 * 7
+
+        const token = jwt.sign({
+            id: user.id
+        }, process.env.JWT_SECRET_KEY, { expiresIn: age })
+
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure : true
+            maxAge: age
+        }).status(200).json({
+            message: "Login successfully"
+        })
+
+    } catch (err) {
+        console.log("err", err)
+        res.status(500).json({
+            message: "Login Failed"
+        })
+    }
+
 }
 
 const logout = (req, res) => {
-    res.send("Logout")
+    res.clearCookie("token").status(200).json({
+        message: "User logout successfully"
+    })
 }
 
 export default { register, login, logout }
